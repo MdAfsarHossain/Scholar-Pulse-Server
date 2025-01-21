8037ae731d8b6c9f15f634a94548fda4687a26b9
MdAfsarHossain <afsar291722@gmail.com>
2025-01-22 00:22:02 +0600

    Delete an applied application data from the db code added

--- index.js
+++ index.js
@@ -482,6 +482,14 @@ async function run() {
       }
     );
 
+    // Delete applied application
+    app.delete("/delete-application/:id", async (req, res) => {
+      const id = req?.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await allApplicationsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
