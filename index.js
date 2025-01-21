4e197ed43672ced14fbe5fda72d632cd6099aa13
MdAfsarHossain <afsar291722@gmail.com>
2025-01-22 00:21:21 +0600

    Delete a single scholarship from the database code added

--- index.js
+++ index.js
@@ -469,6 +469,19 @@ async function run() {
       res.send(result);
     });
 
+    // Delete Specific Scholarships data
+    app.delete(
+      "/scholarship/delete/:id",
+      verifyToken,
+      verifyAdminModerator,
+      async (req, res) => {
+        const id = req?.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await allScholarShipsCollection.deleteOne(query);
+        res.send(result);
+      }
+    );
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
