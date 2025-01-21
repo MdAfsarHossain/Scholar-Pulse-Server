7baeff170b108178b877b2adbf5fd05305fb5b76
MdAfsarHossain <afsar291722@gmail.com>
2025-01-21 23:57:36 +0600

    server site listen code added

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ const jwt = require("jsonwebtoken");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Student Scholarship Management Website!");
+});
+
+app.listen(port, () => {
+  console.log(
+    `Scholarship Management System Website app listening on port ${port}`
+  );
+});
