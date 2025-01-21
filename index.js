e3babdb63483416dc0a10b4dc486314cc17efa0e
MdAfsarHossain <afsar291722@gmail.com>
2025-01-22 00:20:34 +0600

    Delete specific user from the database code added

--- index.js
+++ index.js
@@ -460,6 +460,15 @@ async function run() {
       }
     );
 
+    // ALL DELETED Requests
+    // Delete a user from the database
+    app.delete("/user/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
