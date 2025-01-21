adaf55e42625b9acecbaecf062c2d8937768b146
MdAfsarHossain <afsar291722@gmail.com>
2025-01-22 00:09:03 +0600

    Get single scholarship data form the db code added

--- index.js
+++ index.js
@@ -202,6 +202,14 @@ async function run() {
       }
     );
 
+    // Get single scholarship data from the database
+    app.get("/single-scholartship/:id", verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await allScholarShipsCollection.findOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
