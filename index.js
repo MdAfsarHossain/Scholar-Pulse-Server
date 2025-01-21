eb81ac2c1b1a03da307aa6f9f74f1abbe2586f96
MdAfsarHossain <afsar291722@gmail.com>
2025-01-21 23:58:42 +0600

    Middleware code added

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const port = process.env.PORT || 5000;
 
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://scholarplus-c83e6.web.app",
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+// Middleware
+app.use(cors(corsOptions));
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Welcome to the Student Scholarship Management Website!");
 });
